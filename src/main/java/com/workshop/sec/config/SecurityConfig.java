c6bc5428c9889a5e630f26492c990fff4556c5d8
phagocyte2022 <mp.forcode@gmail.com>
2023-11-27 20:10:53 +0200

    3. Auth page added

---
+++ src/main/java/com/workshop/sec/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.workshop.sec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+}
